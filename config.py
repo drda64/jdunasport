3ed92a0d61bcdad1c1e00c484e92434fa53d20a9
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-26 13:03:22 +0200

    vytvoreni config objektu

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.urandom(32).hex()
+    TEMPLATES_AUTO_RELOAD = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/jdunasport"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
