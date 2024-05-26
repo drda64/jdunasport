29eaad8d6e3409a3871a60fa3d266bd5d72b6fe6
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-26 13:03:05 +0200

    zakladni aplikace

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
