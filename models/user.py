303e22d196e04a7ef4e6a105299d6165518ff854
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-27 12:50:52 +0200

    add db model for user

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
