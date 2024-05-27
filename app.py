c2019bb0c53183d013e1f7ec1011a716a170b072
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-27 12:47:38 +0200

    import of libraries

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from config import Config
+from models import db
+from flask_jwt_extended import JWTManager
+from controllers.create_event_controller import CreateEventController
+from controllers.register_controller import RegisterController
+from controllers.login_controller import LoginController
 
 app = Flask(__name__)
 
