f1471131ef07aca67f801b2eadb5a3768b3435e4
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-27 12:50:24 +0200

    add register controller

---
+++ controllers/register_controller.py
@@ -0,0 +1,30 @@
+from models.user import User
+from flask import request, jsonify
+from flask.views import MethodView
+from schemas.register_schema import RegisterSchema
+from werkzeug.security import generate_password_hash
+from models import db
+from marshmallow.exceptions import ValidationError
+
+
+class RegisterController(MethodView):
+    def post(self):
+        schema = RegisterSchema()
+
+        try:
+            data = schema.load(request.get_json())
+        except ValidationError as e:
+            return jsonify(e.messages), 400
+
+        data = request.get_json()
+        user = User(
+            username=data['username'],
+            email=data['email'],
+            password=generate_password_hash(data['password'])
+        )
+
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({
+            'message': 'User created successfully'
+        })
