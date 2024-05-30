c888f6e4765ac4bbe167142ad0ed3d0bd79d2518
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:12:00 +0200

    chaning config

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+from datetime import timedelta
+
 
 class Config:
     SECRET_KEY = os.urandom(32).hex()
     TEMPLATES_AUTO_RELOAD = True
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/jdunasport"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.urandom(32).hex()
+    JWT_SECRET_KEY = "letsgoooo"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
