a9e96a7b9eb62041339126d3c1e2588e34928f03
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:10:07 +0200

    new routes

--- app.py
+++ app.py
@@ -22,6 +22,20 @@ jwt = JWTManager(app)
 app.add_url_rule('/create_event', view_func=CreateEventController.as_view('create_event'))
 app.add_url_rule('/register', view_func=RegisterController.as_view('register'))
 app.add_url_rule('/login', view_func=LoginController.as_view('login'))
+app.add_url_rule('/sports', view_func=SportsController.as_view('sports'))
+app.add_url_rule('/events', view_func=GetEventsController.as_view('events'))
+app.add_url_rule('/event/<int:event_id>', view_func=GetEventController.as_view('event'))
+app.add_url_rule('/category/<int:event_id>', view_func=GetCategoriesController.as_view('category'))
+app.add_url_rule('/join_event/<int:event_id>', view_func=JoinEventController.as_view('join_event'))
+
+
+@app.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    current_user = get_jwt_identity()
+    new_token = create_access_token(identity=current_user, fresh=False)
+    return {"access_token": new_token}, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=6555)
