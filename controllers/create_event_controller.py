70683ab3d44b8b422d3cdbc436200f90028ee38a
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:12:20 +0200

    add create event controller for creating event

--- controllers/create_event_controller.py
+++ controllers/create_event_controller.py
@@ -1,20 +1,30 @@
 from flask.views import MethodView
 from flask import request, jsonify
+
+from models.category import Category
 from models.event import Event
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class CreateEventController(MethodView):
     @jwt_required()
     def post(self):
         data = request.get_json()
-        event = Event(
-            name=data['name'],
-            date=data['date'],
-            description=data['description'],
-            location=data['location']
-        )
+        print(data)
+        print(get_jwt_identity())
+
+        event = Event()
+        event.name = data['name']
+        event.description = data['description']
+        event.location = data['location']
+        event.event_time = data['event_time']
+        event.user_id = get_jwt_identity()
+        event.sport_id = data['sport_id']
         event.save()
-        return jsonify({
-            'message': 'Event created successfully'
-        })
+
+        # now we save the categories
+        for category in data['categories']:
+            category = Category(name=category["name"], event_id=event.id, capacity=category["capacity"])
+            category.save()
+
+        return "ok"
