166474f05d94dbf76c844a5fc233cf5cad859593
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 16:54:45 +0200

    we check for potential error

--- controllers/get_categories_controller.py
+++ controllers/get_categories_controller.py
@@ -3,10 +3,16 @@ from flask.views import MethodView
 from flask import request, jsonify
 
 from models.category import Category
+from models.event import Event
 
 
 class GetCategoriesController(MethodView):
     def get(self, event_id):
-        print(event_id)
+        # we check if the event exists
+        event = Event.getFirst(id=event_id)
+
+        if not event:
+            return jsonify({'message': 'Event not found'}), 404
+
         categories = Category.query.filter_by(event_id=event_id).all()
         return jsonify(Category.serialize(categories))
