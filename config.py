f12d79db6aa7ef832ca609bbcd6a0b4d43b7fceb
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 18:56:01 +0200

    change db

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from datetime import timedelta
 
 
 class Config:
-    SECRET_KEY = os.urandom(32).hex()
+    SECRET_KEY = "bro"
     TEMPLATES_AUTO_RELOAD = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/jdunasport"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://Drda64:Drda2003@Drda64.mysql.pythonanywhere-services.com/Drda64$jdunasport"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "letsgoooo"
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
