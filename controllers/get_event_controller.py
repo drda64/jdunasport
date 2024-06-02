433be309adb377347285d8d505bda766ab89a208
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 16:54:49 +0200

    we check for potential error

--- controllers/get_event_controller.py
+++ controllers/get_event_controller.py
@@ -3,8 +3,14 @@ from flask.views import MethodView
 from flask import request, jsonify
 
 from models.event import Event
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 class GetEventController(MethodView):
+    @jwt_required()
     def get(self, event_id):
         event = Event.getFirst(id=event_id)
-        return jsonify(Event.to_dict(event))
+
+        if not event:
+            return jsonify({'message': 'Event not found'}), 404
+
+        return jsonify(Event.to_dict(event, get_jwt_identity()))
