7d0b00bb9bfccdf803b6b936413fdeee78bd5a14
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 16:54:57 +0200

    renaming

--- controllers/get_events.py
+++
@@ -1,9 +0,0 @@
-from flask.views import MethodView
-from flask import request, jsonify
-from models.event import Event
-
-
-class GetEventsController(MethodView):
-    def get(self):
-        events = Event.query.all()
-        return jsonify(Event.serialize(events))
