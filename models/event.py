5b3c22d38519055cf3c694b4ca4dfec68c3b66b9
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 16:54:35 +0200

    adding to dict

--- models/event.py
+++ models/event.py
@@ -1,3 +1,5 @@
+from calendar import Calendar
+
 from . import db
 from datetime import datetime
 from .user import User
@@ -7,7 +9,7 @@ from .base_model import BaseModel
 
 class Event(BaseModel):
     __tablename__ = 'events'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, default=lambda: Event.generate_unique_code())
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -18,16 +20,23 @@ class Event(BaseModel):
     user = db.relationship('User', backref=db.backref('events', lazy=True))
     sport = db.relationship('Sport', backref=db.backref('events', lazy=True))
 
-    def to_dict(self):
+    def to_dict(self, user_id):
         # zistime sport, ktery ma id sport_id
         sport = Sport.getFirst(id=self.sport_id)
+        created_by_user = user_id == self.user_id
+
         return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'user_id': self.user_id,
-            'sport': sport.name,
-            'location': self.location,
-            'event_time': self.event_time.isoformat(),
-            'created_at': self.created_at.isoformat()
-        }
+                'id': self.id,
+                'name': self.name,
+                'description': self.description,
+                'user_id': self.user_id,
+                'sport': sport.name,
+                'location': self.location,
+                'event_time': self.event_time.isoformat(),
+                'created_at': self.created_at.isoformat(),
+                'created_by_user': created_by_user
+            }
+    @classmethod
+    def serialize(cls, data, user_id):
+        print("bruh")
+        return [item.to_dict(user_id) for item in data]
